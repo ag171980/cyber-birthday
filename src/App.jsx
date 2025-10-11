7714d6f89a48778edacb97e3c4450de687e36229
ag171980 <ag171980@gmail.com>
2025-10-11 06:11:16 -0300

    Primer push para la web

---
+++ src/App.jsx
@@ -0,0 +1,217 @@
+import Logo from './assets/logo.png'
+import './App.css'
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
+import { useEffect, useState } from 'react'
+
+import Nombre from './assets/nombre.png'
+import MiataTroll from './assets/miata_troll.png'
+import GiftBox from './assets/gift_box.webp'
+
+import video1 from './assets/saludo_troll.mp4'
+import video2 from './assets/saludo.mp4'
+
+import MiataDancing from './assets/miata_twerking_smol.gif'
+
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown'
+import '@leenguyen/react-flip-clock-countdown/dist/index.css'
+
+function App () {
+  const isMobile = window.innerWidth < 600
+  const [targetDate] = useState(new Date('2025-10-13T00:00:00'))
+  const [gifts] = useState([video1, video2])
+  const [stateCount, setStateCount] = useState(false)
+  const [showGift, setShowGift] = useState(undefined)
+  const [showTroll, setShowTroll] = useState(false)
+  const [showSection, setShowSection] = useState(false)
+  const [firstGiftOpened, setFirstGiftOpened] = useState(false)
+  const { width, height } = useWindowSize()
+
+  const abrirPrimerRegalo = () => {
+    setShowGift(gifts[0])
+    setFirstGiftOpened(true)
+  }
+
+  const abrirSegundoRegalo = () => {
+    if (firstGiftOpened) {
+      setShowGift(gifts[1])
+    } else {
+      setShowTroll(true)
+      setTimeout(() => {
+        setShowTroll(false)
+      }, 3000)
+    }
+  }
+
+  useEffect(() => {
+    document.querySelector('.cuenta-regresiva')?.classList.remove('show')
+    document.querySelector('.saludo')?.classList.add('show')
+  }, [stateCount])
+
+  return (
+    <div className='App'>
+      <div className='container'>
+        {!stateCount && <h2>CONTADOR CYBERCUMPLE</h2>}
+        <FlipClockCountdown
+          className='cuenta-regresiva show'
+          to={targetDate}
+          onComplete={() => {
+            setStateCount(true)
+          }}
+          labels={['DÍAS', 'HORAS', 'MINUTOS', 'SEGUNDOS']}
+          digitBlockStyle={
+            isMobile
+              ? {
+                  width: '35px',
+                  height: '45px',
+                  fontSize: '1.7rem',
+                  backgroundColor: '#1a1a1a',
+                  color: '#DBE2E9',
+                  borderRadius: '8px'
+                }
+              : {
+                  width: '80px',
+                  height: '110px',
+                  fontSize: '5.5rem',
+                  backgroundColor: '#1a1a1a',
+                  color: '#DBE2E9',
+                  borderRadius: '8px'
+                }
+          }
+          separatorStyle={
+            isMobile
+              ? { color: '#fff', fontSize: '2rem' }
+              : {
+                  color: '#fff',
+                  fontSize: '4rem'
+                }
+          }
+          labelStyle={
+            isMobile
+              ? { fontSize: '0.7rem', color: '#DBE2E9', marginTop: '0.2rem' }
+              : {
+                  fontSize: '1.3rem',
+                  color: '#DBE2E9',
+                  marginTop: '0.5rem'
+                }
+          }
+        />
+        {stateCount && (
+          <div className='saludo'>
+            <Confetti width={width} height={height} />
+            <div className='carta'>
+              {!showSection && (
+                <>
+                  <img className='logo-principal' src={Logo} alt='' />
+                  <h1>
+                    FELIZ CUMPLEAÑOS <img className='nombre-img' src={Nombre} />
+                    !
+                  </h1>
+                  <br />
+                  <p>
+                    De parte de la <span className='destacados'>CYBERCREW</span>
+                    💜 te queremos mandar un abrazo enorme por tu cumpleaños,
+                    esperamos que pases un excelente día de cumpleaños y que sea
+                    especial.
+                    <br />
+                    <br />
+                    Estamos enormemente agradecidos con vos por haber formado
+                    esta hermosa comunidad de la que tanto los{' '}
+                    <span className='destacados'>NODOS</span>,{' '}
+                    <span className='destacados'>SATURNALES</span> y{' '}
+                    <span className='destacados'>CROMOS</span> disfrutamos día a
+                    día.
+                    <br />
+                    <br />
+                    Sin tu esfuerzo, trabajo y dedicación esto no existiría.
+                    Gracias a vos conocimos personas excelentes con la que
+                    jugamos, hablamos y se crea un buen momento en llamada.
+                    <br />
+                    <br />
+                    La idea no es ponerse sentimentales, pero creemos que cada
+                    tanto es lindo recordarte lo que significas para nosotros,
+                    tanto vos como tu trabajo.
+                    <br />
+                    <br />A continuación, te dejamos 2 regalitos (aparte del que
+                    esta llegando 🚚...) para que abras.
+                  </p>
+                  <div className='bottom'>
+                    <button
+                      className='btn'
+                      onClick={() => setShowSection(true)}
+                    >
+                      Ver Regalos
+                    </button>
+                  </div>
+                </>
+              )}
+
+              {showSection && (
+                <>
+                  <img src={MiataDancing} alt='' />
+                  <h3>Hace click en los regalos para abrirlos</h3>
+                  <div className='regalos'>
+                    <div className='regalo'>
+                      <h4>Regalo 1</h4>
+                      <img
+                        onClick={() => abrirPrimerRegalo()}
+                        className='regalo-box'
+                        src={GiftBox}
+                        alt=''
+                      />
+                    </div>
+                    <div className='regalo'>
+                      <h4>Regalo 2</h4>
+                      <img
+                        onClick={() => abrirSegundoRegalo()}
+                        className='regalo-box'
+                        src={GiftBox}
+                        alt=''
+                      />
+                    </div>
+                  </div>
+
+                  {showGift && (
+                    <div className='ver-regalos'>
+                      <button
+                        onClick={() => setShowGift(undefined)}
+                        className='btn-close'
+                      >
+                        X
+                      </button>
+                      <video
+                        preload='none'
+                        className='video'
+                        src={showGift}
+                        controls
+                      ></video>
+                      <small>
+                        ⚠️Advertencia⚠️: Bajale un poco el volumen, no nos
+                        hacemos cargo de daños en los tímpanos.
+                      </small>
+                    </div>
+                  )}
+                  {showTroll && (
+                    <div>
+                      <p>Tenés que abrir el primer regalo </p>
+                      <div className='troll'>
+                        <img className='miata-troll' src={MiataTroll} alt='' />
+                        <img className='miata-troll' src={MiataTroll} alt='' />
+                        <img className='miata-troll' src={MiataTroll} alt='' />
+                      </div>
+                    </div>
+                  )}
+                  <button className='btn' onClick={() => setShowSection(false)}>
+                    Volver
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
